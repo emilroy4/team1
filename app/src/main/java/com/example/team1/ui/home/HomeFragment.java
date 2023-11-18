a2c45f2445fc84ad21e9923175a9b2075904f8d7
emilroy4 <emilroy100@gmail.com>
2023-11-18 12:02:24 +0000

    finished map

--- app/src/main/java/com/example/team1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/team1/ui/home/HomeFragment.java
@@ -5,42 +5,61 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.team1.R;
 import com.example.team1.databinding.FragmentHomeBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-    private HomeViewModel homeViewModel;
     private MapView mapView;
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
 
-        // Initialize ViewModel
-        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        // Set up TextView
-        final TextView textView = root.findViewById(R.id.text_home);
-        final TextView textView1 = root.findViewById(R.id.textView2);
-
+        final TextView textView = binding.textHome;
+        final TextView locationTextView = binding.textView2;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        homeViewModel.getLocationText().observe(getViewLifecycleOwner(), locationTextView::setText);
 
 
 
-
-        // Set up MapView
+        // Initialize the MapView
         mapView = root.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                // Add a marker for a specific location
+                LatLng location = new LatLng(53.38487296112957, -6.2581844182077475); // Replace with your desired coordinates
+                googleMap.addMarker(new MarkerOptions().position(location).title("Training location"));
+                // Move camera to the specific location
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12.0f));
+            }
+        });
 
         return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
     @Override
@@ -65,17 +84,5 @@ public class HomeFragment extends Fragment {
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
     }
 }
--- app/src/main/java/com/example/team1/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/team1/ui/home/HomeViewModel.java
@@ -7,15 +7,26 @@ import androidx.lifecycle.ViewModel;
 public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> locationText;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
 
         mText.setValue("Announcement:");
 
+        locationText = new MutableLiveData<>();
+        locationText.setValue("Location:");
+
     }
+
 
     public LiveData<String> getText() {
         return mText;
     }
+
+
+    public LiveData<String> getLocationText() {
+        return locationText;
+    }
+
 }
